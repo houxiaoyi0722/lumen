07fb5fa279189b4c47391dc73212dec31f9a3857
hxy <xiaoyihou@outlook.com>
2021-05-14 00:13:46 +0800

    System 模块

---
+++ system/src/test/java/com/sang/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
