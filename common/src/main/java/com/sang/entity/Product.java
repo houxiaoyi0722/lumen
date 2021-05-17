2de75ff8c89d0f31cb256941f988274c0dbb67c3
hxy <xiaoyihou@outlook.com>
2021-05-17 23:29:47 +0800

    lombok

--- common/src/main/java/com/sang/entity/Product.java
+++ common/src/main/java/com/sang/entity/Product.java
@@ -32,50 +32,9 @@ public class Product {
   @Size(max = 50)
   String name;
 
-  public Product(String sku, String name) {
-    this.sku = sku;
-    this.name = name;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  /**
-   * Return sku.
-   */
-  public String getSku() {
-    return sku;
-  }
-
-  /**
-   * Set sku.
-   */
-  public void setSku(String sku) {
-    this.sku = sku;
-  }
-
-  /**
-   * Return name.
-   */
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * Set name.
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public static Product byName(String name) {
     return new QProduct()
-      .name.equalTo(name)
-      .findOne();
+            .name.equalTo(name)
+            .findOne();
   }
 }
