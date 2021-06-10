835ddfaa2a0513a25a569e3df1667218dbb5e8db
hxy <xiaoyihou@outlook.com>
2021-06-11 00:19:09 +0800

    数据字典

--- system/src/test/java/com/sang/system/SystemApplicationTests.java
+++ system/src/test/java/com/sang/system/SystemApplicationTests.java
@@ -13,13 +13,13 @@ class SystemApplicationTests {
 
     @Test
     void contextLoads() {
-        DataDictionary build = DataDictionary.builder().groupKey("11").groupValue("22").build();
-        DataDictionaryItem build1 = DataDictionaryItem.builder().itemKey("11").itemValue("132").build();
-        ArrayList<DataDictionaryItem> objects = new ArrayList<>();
-        objects.add(build1);
-        build.setDataDictionaryItems(objects);
-
-        DB.save(build);
+//        DataDictionary build = DataDictionary.builder().groupKey("11").groupValue("22").build();
+//        DataDictionaryItem build1 = DataDictionaryItem.builder().itemKey("11").itemValue("132").build();
+//        ArrayList<DataDictionaryItem> objects = new ArrayList<>();
+//        objects.add(build1);
+//        build.setDataDictionaryItems(objects);
+//
+//        DB.save(build);
     }
 
 }
