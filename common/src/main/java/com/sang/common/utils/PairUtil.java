082379dbb5ab73b43513b6b4bca715f003d6859b
houxiaoyi0722 <47712396+houxiaoyi0722@users.noreply.github.com>
2021-12-31 15:29:58 +0800

    rsa密钥对生成工具

---
+++ common/src/main/java/com/sang/common/utils/PairUtil.java
@@ -0,0 +1,76 @@
+package com.sang.common.utils;
+
+import cn.hutool.core.codec.Base64Decoder;
+import cn.hutool.core.codec.Base64Encoder;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * rsa密钥对生成工具
+ * @author hxy
+ * @date 2021/12/30 18:31
+ **/
+public class PairUtil {
+
+    public static final String KEY_ALGORITHM = "RSA";
+    public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
+    private static final String PUBLIC_KEY = "RSAPublicKey";
+    private static final String PRIVATE_KEY = "RSAPrivateKey";
+
+    public static String getPublicKey(Map<String, Object> keyMap) throws Exception {
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return encryptBASE64(key.getEncoded());
+    }
+
+    public static String getPrivateKey(Map<String, Object> keyMap) throws Exception {
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return encryptBASE64(key.getEncoded());
+    }
+
+    public static byte[] decryptBASE64(String key) throws Exception {
+        return Base64Decoder.decode(key);
+    }
+
+    public static String encryptBASE64(byte[] key) throws Exception {
+        return Base64Encoder.encode(key);
+    }
+
+    public static Map<String, Object> initKey() throws Exception {
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        keyPairGen.initialize(1024);
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        Map<String, Object> keyMap = new HashMap<String, Object>(2);
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+        return keyMap;
+
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> keyMap;
+        try {
+            keyMap = initKey();
+            String publicKey = getPublicKey(keyMap);
+
+            System.out.println("-----BEGIN PUBLIC KEY-----");
+            System.out.println(publicKey);
+            System.out.println("-----END PUBLIC KEY-----");
+            String privateKey = getPrivateKey(keyMap);
+            System.out.println("-----BEGIN PRIVATE KEY-----");
+            System.out.println(privateKey);
+            System.out.println("-----END PRIVATE KEY-----");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
