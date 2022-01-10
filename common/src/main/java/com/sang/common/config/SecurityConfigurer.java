3bc6d0f8de496f7ef7cb8d9761e62fa1a8f0922c
houxiaoyi0722 <47712396+houxiaoyi0722@users.noreply.github.com>
2022-01-10 18:32:35 +0800

    SecurityConfigurer.java

--- common/src/main/java/com/sang/common/config/SecurityConfigurer.java
+++ common/src/main/java/com/sang/common/config/SecurityConfigurer.java
@@ -1,8 +1,10 @@
 package com.sang.common.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +21,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, // 允许对于网站静态资源的无授权访问
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/swagger-ui.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/swagger-resources/**",
+                        "/v2/api-docs/**",
+                        "/webjars/**",
+                        "/csrf",
+                        "/v1/healthz",
+                        "/actuator/**"
+                ).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()//跨域请求会先进行一次options请求
+                .antMatchers("/**").permitAll() //白名单
+                .anyRequest().authenticated()// 除上面外的所有请求全部需要鉴权认证
+                //自定义决策管理器accessDecisionManager，使用自定义AccessDecisionVoter
+                //.accessDecisionManager(accessDecisionManager())
+                .and().cors().disable();
     }
 }
