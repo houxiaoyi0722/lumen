79dc3b2525f7e99a595289449642683d6601eeab
houxiaoyi0722 <47712396+houxiaoyi0722@users.noreply.github.com>
2022-02-09 10:18:58 +0800

    ThreadLocalRandomExample.java

---
+++ system/src/test/java/com/sang/system/example/juc/ThreadLocalRandomExample.java
@@ -0,0 +1,41 @@
+package com.sang.system.example.juc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.*;
+
+/**
+ * @author hxy
+ * @date 2022/2/8 17:28
+ **/
+public class ThreadLocalRandomExample {
+
+    @Test
+    public void test() throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,5,1000, TimeUnit.SECONDS,new ArrayBlockingQueue<>(10), Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
+
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+
+        for (int i = 0; i < 5; i++) {
+            threadPoolExecutor.execute(() -> {
+                for (int j = 0; j < 10; j++) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " : " +  ThreadLocalRandom.current().nextInt(10) + " J : " + j);
+                }
+                countDownLatch.countDown();
+            });
+        }
+/*        try {
+            boolean b = threadPoolExecutor.awaitTermination(1000, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        countDownLatch.await();
+        threadPoolExecutor.shutdown();
+    }
+
+}
