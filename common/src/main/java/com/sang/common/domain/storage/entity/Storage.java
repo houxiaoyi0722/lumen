90edceb3e6d7a1fc80f1c7f228a2939eceb68c92
houxiaoyi <xiaoyihou@outlook.com>
2022-06-22 17:22:57 +0800

    minio:docker-compose,entity

---
+++ common/src/main/java/com/sang/common/domain/storage/entity/Storage.java
@@ -0,0 +1,24 @@
+package com.sang.common.domain.storage.entity;
+
+
+import com.sang.common.domain.base.entity.BaseModel;
+import io.ebean.annotation.DbComment;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "STORAGE")
+@DbComment("对象存储管理")
+public class Storage extends BaseModel {
+
+
+
+
+}
