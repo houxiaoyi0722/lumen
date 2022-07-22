c0dfc165944d3021acdde76139eea59d53890c93
houxiaoyi <xiaoyihou@outlook.com>
2022-07-22 16:06:04 +0800

    spring event

--- system/src/test/java/com/sang/system/example/spring/eventDemo/event/UserRegisterEvent.java
+++ system/src/test/java/com/sang/system/example/spring/eventDemo/event/UserRegisterEvent.java
@@ -1,21 +1,28 @@
 package com.sang.system.example.spring.eventDemo.event;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.context.ApplicationEvent;
 
 /**
  * 用户注册事件
  */
-@AllArgsConstructor
-@Getter
-@Setter
 public class UserRegisterEvent extends ApplicationEvent {
 
     /**
      * 用户名
      */
     private String username;
+
+    public UserRegisterEvent(Object source) {
+        super(source);
+    }
+
+    public UserRegisterEvent(Object source, String username) {
+        super(source);
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 
 }
