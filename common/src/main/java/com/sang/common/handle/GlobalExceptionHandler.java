6bec27d5da4c23bcb19dbc244168dab7a2319548
houxiaoyi <xiaoyihou@outlook.com>
2022-08-19 16:43:34 +0800

    GlobalExceptionHandler

--- common/src/main/java/com/sang/common/handle/GlobalExceptionHandler.java
+++ common/src/main/java/com/sang/common/handle/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import com.sang.common.exception.BusinessException;
 import com.sang.common.response.Result;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -25,10 +26,8 @@ import javax.servlet.http.HttpServletRequest;
  * @author hxy
  */
 @RestControllerAdvice(annotations = RestController.class)
-@Log4j2
+@Slf4j
 public class GlobalExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
     /**
      * BusinessException 异常发生时，打印日志信息, 请求状态200
      * @param e 异常
@@ -37,10 +36,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     @ResponseBody
     public ResponseEntity<Result> handlerBusinessException(BusinessException e, HttpServletRequest request) {
-        // 异常发生时，打印ERROR级别的日志信息
-        e.printStackTrace();
-        printLog(request, ExceptionUtil.stacktraceToString(e));
-
         return new ResponseEntity<>(Result.error(e.getMessage(), e.getCode()), HttpStatus.OK);
     }
 
@@ -52,10 +47,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     @ResponseBody
     public ResponseEntity<Result> handlerBusinessException(BaseException e, HttpServletRequest request) {
-        // 异常发生时，打印ERROR级别的日志信息
-        e.printStackTrace();
-        printLog(request, ExceptionUtil.stacktraceToString(e));
-
         return new ResponseEntity<>(Result.error(e.getMessage(), e.getCode()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
@@ -67,31 +58,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseEntity<Result> handlerAllException(Exception e, HttpServletRequest request) {
-        // 异常发生时，打印ERROR级别的日志信息
-        e.printStackTrace();
-        printLog(request, ExceptionUtil.stacktraceToString(e));
-
         HttpStatus errorStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(Result.error(e.getMessage(), errorStatus.value()), errorStatus);
-    }
-
-    @Data
-    public class RequestErrorInfo {
-        //        private String ip;
-        private String userName;
-        private String url;
-        private String httpMethod;
-        private String deviceId;
-        private String exception;
-    }
-
-    private void printLog(HttpServletRequest request, String stackTrace) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        RequestErrorInfo requestErrorInfo = new RequestErrorInfo();
-        requestErrorInfo.setUserName(authentication.getPrincipal().toString());
-        requestErrorInfo.setUrl(request.getRequestURL().toString());
-        requestErrorInfo.setException(stackTrace);
-        logger.error("Error Request Info      : {}", JSONUtil.toJsonStr(requestErrorInfo));
     }
 
 }
