4461c993186a3f4a439b311ede9c4cc733fba048
houxiaoyi <xiaoyihou@outlook.com>
2022-11-30 13:50:41 +0800

    策略模式实现

--- system/src/test/java/com/sang/system/example/spring/multipleAutoWired/ShapeBeanFactory.java
+++ system/src/test/java/com/sang/system/example/spring/multipleAutoWired/ShapeBeanFactory.java
@@ -8,13 +8,13 @@ import java.util.Map;
 
 @Component
 public class ShapeBeanFactory {
-
     @Autowired//关键在这个，原理：当一个接口有多个实现类的时候，key为实现类名，value为实现类对象
     private Map<String, Shape> shapeMap;
 
     @Autowired//这个注入了多个实现类对象
     private List<Shape> shapeList;
 
+    // 策略模式实现 -- 根据业务逻辑调用不同类
     public Shape getShape(String shapeType) {
         Shape bean1 = shapeMap.get(shapeType);
         System.out.println(bean1);
