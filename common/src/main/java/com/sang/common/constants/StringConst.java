244cf547ae7f3f56e6ddb8fd9f6c2bdadcf8eb52
houxiaoyi <xiaoyihou@outlook.com>
2023-06-20 18:07:50 +0800

    flowable 用户相关模块

--- common/src/main/java/com/sang/common/constants/StringConst.java
+++ common/src/main/java/com/sang/common/constants/StringConst.java
@@ -5,6 +5,7 @@ public class StringConst {
     public static final String EMPTY = "";
     public static final String DOT = ".";
     public static final String COMMA = ",";
+    public static final String PERCENT_SIGN = "%";
     public static final String FORWARD_SLASH = "/";
     public static final String BACKWARD_SLASH = "\\";
     public static final String JOB_LOG_ID = "jobLogId";
