8672cf10c6d7b3230f71ae9e8cc7d0e340d7f78c
hxy <xiaoyihou@outlook.com>
2023-09-07 16:16:53 +0800

    获取流程图

--- flowable/src/main/java/com/sang/flowable/controller/flowable/FlowableProcessManageController.java
+++ flowable/src/main/java/com/sang/flowable/controller/flowable/FlowableProcessManageController.java
@@ -1,43 +1,39 @@
 package com.sang.flowable.controller.flowable;
 
 import cn.hutool.core.util.StrUtil;
-import cn.smallbun.screw.core.util.CollectionUtils;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sang.common.constants.FlowableConst;
 import com.sang.common.constants.StringConst;
 import com.sang.common.domain.auth.authentication.user.repo.UserGroupRepository;
 import com.sang.common.domain.auth.authentication.user.repo.UserRepository;
 import com.sang.common.response.Result;
 import com.sang.common.utils.ResponseUtil;
-import com.sang.flowable.dto.*;
 import com.sang.flowable.param.SuspendedActiveParam;
-import com.sang.flowable.service.debugger.DebuggerService;
 import com.sang.flowable.service.flowable.FlowableProcessManageService;
 import lombok.extern.slf4j.Slf4j;
-import org.flowable.bpmn.model.*;
+import org.flowable.bpmn.model.BpmnModel;
 import org.flowable.common.engine.api.FlowableException;
-import org.flowable.engine.*;
-import org.flowable.engine.history.HistoricActivityInstance;
+import org.flowable.engine.IdentityService;
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.repository.Deployment;
-import org.flowable.engine.runtime.Execution;
-import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.idm.api.Group;
 import org.flowable.idm.api.GroupQuery;
 import org.flowable.idm.api.User;
 import org.flowable.idm.api.UserQuery;
-import org.flowable.job.api.Job;
+import org.flowable.image.impl.DefaultProcessDiagramGenerator;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * flowable 流程管理controller
@@ -177,6 +173,29 @@ public class FlowableProcessManageController {
 
         userRepository.findAll().forEach(flowableProcessManageService::updateUser);
         return Result.ok();
+    }
+
+    @Resource
+    private ProcessEngine processEngine;
+
+    /**
+     * 获取流程图例
+     * @param processDefinitionId
+     * @param processInstanceId
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("resource-view")
+    public void resourceView(@RequestParam String processDefinitionId, String processInstanceId, HttpServletResponse response) throws Exception {
+        // 获取流程图
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinitionId);
+        ProcessEngineConfiguration engConf = processEngine.getProcessEngineConfiguration();
+
+//        ProcessDiagramGenerator diagramGenerator = new CustomProcessDiagramGenerator();
+        DefaultProcessDiagramGenerator diagramGenerator = new DefaultProcessDiagramGenerator();
+        InputStream in = diagramGenerator.generateDiagram(bpmnModel, "bmp", Collections.emptyList(), Collections.emptyList(), engConf.getActivityFontName(),
+                engConf.getLabelFontName(), engConf.getAnnotationFontName(), engConf.getClassLoader(), 1.0, true);
+        ResponseUtil.sendStream(in,"image",processInstanceId + ".png");
     }
 
     /**
