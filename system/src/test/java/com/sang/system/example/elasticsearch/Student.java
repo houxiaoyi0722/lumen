efb59d742d28bf30f2a3349c818c3ca67b964985
houxiaoyi <xiaoyihou@outlook.com>
2023-11-02 17:33:49 +0800

    elasticsearch

--- system/src/test/java/com/sang/system/example/elasticsearch/Student.java
+++ system/src/test/java/com/sang/system/example/elasticsearch/Student.java
@@ -12,8 +12,12 @@ import javax.persistence.Id;
 public class Student {
     @Id
     private String id;
-    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_max_word")
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String name;
-    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_max_word")
+    /**
+     * 指定字段类型、分词器、查询分词器
+     * index = false 表示不索引该字段，无法通过该字段查询数据
+     */
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart",index = false)
     private String price;
 }
