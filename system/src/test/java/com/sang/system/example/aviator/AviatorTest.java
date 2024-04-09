a5ee61ff6c9f2dcd523d7bffbad1dae08d8195b8
houxiaoyi <xiaoyihou@outlook.com>
2024-04-09 10:59:58 +0800

    sonar-scan

--- system/src/test/java/com/sang/system/example/aviator/AviatorTest.java
+++ system/src/test/java/com/sang/system/example/aviator/AviatorTest.java
@@ -13,19 +13,22 @@ public class AviatorTest {
     public void test() {
 
         Map<String, Object> value = new HashMap<>();
-        value.put("a",10);
+        value.put("a",null);
         value.put("b",5);
-        value.put("veh_model_name","aaa");
-        value.put("config_name","5555定时");
+        value.put("veh_model_name","veh_model_name");
+        value.put("config_name","config_name");
 
-        Expression compiledExp = AviatorEvaluator.compile("a*10 + '测试'",true);
-        Expression compiledExp2 = AviatorEvaluator.compile("veh_model_name + config_name",true);
+//        Expression compiledExp = AviatorEvaluator.compile("a*10 + '测试'",true);
+        Expression compiledExp2 = AviatorEvaluator.compile("veh_model_name + '_' + config_name",true);
+        Expression compiledExp3 = AviatorEvaluator.compile("a != '' ? (veh_model_name + a) : (veh_model_name + b)",true);
         // Execute with injected variables.
-        String result = (String) compiledExp.execute(value);
+//        String result = (String) compiledExp.execute(value);
         String result2 = (String) compiledExp2.execute(value);
+        String result3 = (String) compiledExp3.execute(value);
 
-        System.out.println(result);
+//        System.out.println(result);
         System.out.println(result2);
+        System.out.println(result3);
     }
 
 
