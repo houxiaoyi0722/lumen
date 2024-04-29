50ad733cacea40850a96e73e0d807ae71b3d2807
houxiaoyi <xiaoyihou@outlook.com>
2024-04-29 09:19:13 +0800

    rocketmq example

--- system/src/main/java/com/sang/system/mq/TestConsumer.java
+++ system/src/main/java/com/sang/system/mq/TestConsumer.java
@@ -1,18 +1,13 @@
 package com.sang.system.mq;
 
-import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
-//@RocketMQMessageListener(topic = "msp-test", consumerGroup = "consumer-group-test", selectorExpression = "tag1 || tag2")
-
-// 顺序消费 全局队列锁防止其他消费者消费+消费者队列对象锁防止消费者内部其他线程消费+process队列锁防止队列重新分配
 @RocketMQMessageListener(
-        topic = "msp-test",
-        consumerGroup = "consumer-group-test",
-        selectorExpression = "tag1 || tag2",
-        consumeMode = ConsumeMode.ORDERLY)
+        topic = "bdmp-notice",
+        consumerGroup = "",
+        selectorExpression = "tag1 || tag2")
 @Component
 public class TestConsumer implements RocketMQListener<String> {
 
